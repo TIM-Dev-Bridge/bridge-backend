e556fa18f9388f27929ad21162c816f7448c5bc3
Marks-manship <60937610+Marks-manship@users.noreply.github.com>
2021-11-09 23:12:12 +0700

    UPDATE: user database schema

--- model/user.js
+++ model/user.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  //can use
+  // first_name: { type: String, default: null },
+  // last_name: { type: String, default: null },
+  // email: { type: String, unique: true },
+  // password: { type: String },
+  // token: { type: String },
+  //new one
   first_name: { type: String, default: null },
   last_name: { type: String, default: null },
+  display_name: { type: String, default: null },
+  birth_date: { type: String, default: null },
+  access: {
+    permission: "user",
+  },
   email: { type: String, unique: true },
   password: { type: String },
   token: { type: String },
