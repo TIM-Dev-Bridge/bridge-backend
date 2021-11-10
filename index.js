fd58450c531cfeef567538c92d6819d9952c8c88
Marks-manship <60937610+Marks-manship@users.noreply.github.com>
2021-11-10 22:05:41 +0700

    UPDATE: get the tournament time

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
 //Authen
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const config = process.env;
 
 //----------------------------Database----------------------------//
 const TourR = require("./model/tourR");
+const { log } = require("console");
 
 let users = [];
 
@@ -25,26 +26,38 @@ let users = [];
 //   } catch (err) {}
 // });
 
-io.use(function(socket, next){
-  if (socket.handshake.query && socket.handshake.query.token){
-    console.log('pass');
+io.use(function (socket, next) {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    console.log("pass");
     // console.log(socket.handshake.query);
     console.log(socket.handshake.query.token);
-    jwt.verify(socket.handshake.query.token, config.TOKEN_KEY, function(err, decoded) {
-      if (err) return next(new Error('Authentication error'));
-      socket.decoded = decoded;
-      next();
-    });
+    jwt.verify(
+      socket.handshake.query.token,
+      config.TOKEN_KEY,
+      function (err, decoded) {
+        if (err) return next(new Error("Authentication error"));
+        socket.decoded = decoded;
+        next();
+      }
+    );
+  } else {
+    next(new Error("Authentication error"));
   }
-  else {
-    next(new Error('Authentication error'));
-  }    
-})
+});
 
 io.on("connection", (socket) => {
   console.log("A new user connected");
   console.log(socket.id);
-  console.log('Data of user is ' + socket.handshake.query.userName);
+  console.log("decode = " + JSON.stringify(socket.decoded));
+  console.log("Data of user is " + socket.handshake.query.userName);
+  console.log("gettime", socket.handshake.getTime);
+
+  //test time
+  //socket.emit('datetime', { datetime: new Date().getTime() });
+  let timer = new Date().toLocaleString("en-US", {timeZone: "Asia/Jakarta"});
+  console.log("timer", timer);
+    //find way to 
+    //set time out (millisec)
   //Save session
 
   //Join socket io server
@@ -57,12 +70,37 @@ io.on("connection", (socket) => {
     io.emit("new user", users);
     console.log(users);
   });
+  //Join tour
+  socket.on("join-tour", async (player_name, tour_name) => {
+    socket.join(tour_name);
+    console.log(`username ${player_name} is join the ${tour_name} tour`);
+    //Add user to tour
+    try {
+      const hasTour = await TourR.findOne({ tour_name });
+      if (!hasTour) {
+        socket.emit("join-tour", "This tour is not found");
+      }
+      // if player < 20 Condition & not prime number
+      const joinTour = await TourR.updateOne(
+        { tour_name: tour_name },
+        { $push: { player_name: player_name } }
+      );
+
+      //Send response to client
+      socket.emit("join-tour", joinTour);
+    } catch (error) {
+      console.log("error");
+      console.log(error);
+    }
+    //Send response to client
+    socket.emit("join-tour", `${player_name} connected Server`);
+  });
   //Join room
-  socket.on("join-room",  (user, roomName) => {
+  socket.on("join-room", (user, roomName) => {
     socket.join(roomName);
     console.log(`username ${user} is join the ${roomName} room`);
     //Send response to client
-    socket.emit('join-room',`${user} connected Server`)
+    socket.emit("join-room", `${user} connected Server`);
     // socket.on("join-team",  (user, team) => {
     //   socket.join(team);
     //   console.log(`username ${user} is join the ${team} team`);
@@ -86,7 +124,7 @@ io.on("connection", (socket) => {
     //Find player
   });
   //Join team
-  socket.on("join-team",  (user, team) => {
+  socket.on("join-team", (user, team) => {
     socket.join(team);
     console.log(`username ${user} is join the ${team} team`);
   });
