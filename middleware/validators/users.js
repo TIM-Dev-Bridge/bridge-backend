5fa755e0e183749574ab7f0134a45832c28af365
CryoHerz <icemintpon@hotmail.com>
2022-02-17 19:25:07 +0700

    Regex Fix

--- middleware/validators/users.js
+++ middleware/validators/users.js
@@ -1,10 +1,3 @@
-const isEmail = (email) => {
-  const regEx =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(regEx)) return true;
-  else return false;
-};
-
 exports.validateSignupData = (data) => {
   let errors = {};
   //Validate user input
@@ -30,28 +23,33 @@ exports.validateSignupData = (data) => {
     errors.last_name = "Lastname only accept alphabet";
   }
   //TS_SU_12-14
-  if (!isEmail(data.email)) {
+  if (!data.email.match(/^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$/)) {
     errors.email = "Invalid email format";
   }
-  //TC_SU_16-17
-  if (!data.display_name.match(/^ (?=.{5,16})$/)) {
-    errors.display_name = "Length of displayname should between 5 and 16";
+  //TC_SU_16
+  if (!data.display_name.match(/^.{5,}$/)) {
+    errors.display_name = "Display Name should contain at least 5 characters";
+  }
+  //TC_SU_17
+  if (!data.display_name.match(/^.{,16}$/)) {
+    errors.display_name = "Display Name should contain less than 16 characters";
   }
   //TC_SU_22
   if (
-    !data.password.match(
-      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]$/
-    )
-  ) {
-    errors.format_password =
-      "Password should include Capital Letter, small Letter, number and special character";
+    !data.password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[ !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]).*$/)) {
+    errors.format_password = "Password should include capital letter, small Letter, number and special character";
+  }
+  //TC_SU_XX {Pending}
+  if (
+    !data.password.match(/^(?!.*[^a-zA-Z0-9 !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]).*$/)) {
+    errors.format_password = "Password should not contain invalid character";
   }
   //TC_SU_23
-  if (data.password.match(/^ (?=.{8,})$/)) {
+  if (data.password.match(/^.{8,}$/)) {
     errors.password_short = "Password should contain at least 8 characters";
   }
   //TC_SU_24
-  if (data.password.match(/^ (?=.{,32})$/)) {
+  if (data.password.match(/^.{,32}$/)) {
     errors.password_long = "Password should contain less than 32 character";
   }
   //TC_SU_25 match with confirmed password
@@ -67,16 +65,16 @@ exports.validateSignupData = (data) => {
 exports.validateLoginData = (data) => {
   let errors = {};
   //TC_LI_01
-  if (!data.email) errors.email = "Must not be empty";
-  if (!data.password) errors.password = "Must not be empty";
+  // if (!data.email) errors.email = "Must not be empty";
+  // if (!data.password) errors.password = "Must not be empty";
   //TC_LI_05-07
-  if (!isEmail(data.email)) {
-    errors.format_email = "Invalid email format";
-  }
+  // if (!isEmail(data.email)) {
+  //   errors.format_email = "Invalid email format";
+  // }
   //TC_LI_10-11
-  if (!data.password.match(/^ (?=.{8,32})$/)) {
-    errors.format_password = "Invalid Password";
-  }
+  // if (!data.password.match(/^ (?=.{8,32})$/)) {
+  //   errors.format_password = "Invalid Password";
+  // }
 
   return {
     errors,
